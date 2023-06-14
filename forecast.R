69f6dcb12b9c6665d74b12f70b2c3917ca40aad3
Michael Dietze <dietze@bu.edu>
2023-06-14 16:49:42 -0400

    update to forecast. Doesn't run yet because code not set up for ensemble met (yet)

--- forecast.R
+++ forecast.R
@@ -1,63 +1,75 @@
 ## Configuration
 library(compiler)
 library(tidyverse)
-library(mvtnorm)
-library(EML)
+#library(mvtnorm)
+#library(EML)
 source("R/functions.R")
 ne = 500 ## production run should be 200 - 5000, depending on what your computer can handle
-
 timestep = 1800 #seconds
 
-#today = Sys.time()
+## Get previous Analysis
+Analysis.files = dir("analysis")
+last = length(Analysis.files)
+last.date = substr(Analysis.files[last],1,10)
+Analysis = readRDS(file.path("analysis",Analysis.files[last]))
 
+#today = Sys.time()
 today = "2022-07-01 00:00:00"
-jumpBack = 5  ## how many days do we go back to account for data latency?
+today_timestamp = strptime(today, "%Y-%m-%d %H:%M:%S",tz="UTC")
+today_ch        = as.character(as.Date(today))
+jumpBack = min(60,max(5,as.Date(today) - as.Date(last.date)))  ## how many days do we go back to account for data latency?
+start_date = lubridate::as_datetime(today)-lubridate::days(jumpBack)
 horiz       = 35 #days, forecast horizon during forecast
 
+## Get latest increment of data (flux)
+target <- readr::read_csv("https://data.ecoforecast.org/neon4cast-targets/terrestrial_30min/terrestrial_30min-targets.csv.gz", guess_max = 1e6) %>%
+  dplyr::filter(site_id == "NIWO",datetime >= start_date)
 
-## Get latest increment of data (flux & met)
-target <- arrow::s3_bucket("targets/terrestrial_30min", endpoint_override="data.ecoforecast.org") %>% 
-  dplyr::filter(site_id == "NIWO",time >= as.Date(today)-jumpBack) %>%
-  dplyr::collect()
-
-## build dat for Analysis  ******
-#nep = -flux$NEE_VUT_REF[today]
-#nep.qc = flux$NEE_VUT_REF_QC[today]
-nep[nep.qc>0] = NA
-dat = data.frame(nep = nep,
-                 sigma.nep = flux$NEE_VUT_REF_JOINTUNC[today]
+## build dat for Analysis
+nee = target |> filter(variable == "nee")
+fu.params = flux.uncertainty(nee$observation)
+unc = predict.flux.uncertainty(nee$observation,fu.params)
+dat = data.frame(nep = -nee$observation,
+                 sigma.nep = unc
 )
 
 ##  Get weather forecast
-Sys.unsetenv("AWS_DEFAULT_REGION")
-Sys.unsetenv("AWS_S3_ENDPOINT")
-Sys.setenv(AWS_EC2_METADATA_DISABLED="TRUE")
-s3 <- arrow::s3_bucket("drivers/noaa/neon/gefs", 
-                       endpoint_override =  "js2.jetstream-cloud.org:8001",
-                       anonymous=TRUE)
-df <- arrow::open_dataset(s3)
-dc <- df %>% 
-  filter(site_id == "NIWO",start_time >= as.Date(today)-jumpBack, variable %in% c("TMP","DSWRF")) %>%
-  collect()
-
-## for reforecast, stitch together last observed met and first day of each forecast
-
-## for forecast, grab matching start_time
-met <- dc %>% filter(start_time == as.Date(today)) %>% pivot_wider(names_from = ensemble,values_from = predicted)
-PAR = met %>% filter(variable == "DSWRF") 
+dc = neon4cast::noaa_stage2() |>
+  dplyr::filter(site_id == "NIWO",
+                variable %in% c("air_temperature","surface_downwelling_shortwave_flux_in_air"),
+                start_date == today_ch) |>
+  dplyr::collect()
+met <- dc  %>% pivot_wider(names_from = parameter,values_from = prediction)
+PAR = met %>% filter(variable == "surface_downwelling_shortwave_flux_in_air") 
 inputs <- list(
-  date = PAR$time,
-  temp = temp = met %>% filter(variable == "TMP") %>% head(-1) %>% select(-(1:9)) %>% t(), ## air temperature (Celsius)
-  PAR  = as.matrix(t(PAR[,-(1:9)])) / 0.486 ## gap filled PAR, conversion From Campbell and Norman p151
+  date = PAR$datetime,
+  temp = met %>% filter(variable == "air_temperature") %>% select(-(1:11)) %>% t()-273.15, ## air temperature (Celsius) ## prev included %>% head(-1)
+  PAR  = as.matrix(t(PAR[,-(1:11)])) / 0.486 ## gap filled PAR, conversion From Campbell and Norman p151
 )
 
-## Get previous Analysis
-Analysis = readRDS("Analysis.RDS")
-last = length(Analysis)
+## reforecast met: stitch together first day of each weather forecast
+dr = neon4cast::noaa_stage3() |>
+  dplyr::filter(site_id == "NIWO",
+                  variable %in% c("air_temperature","surface_downwelling_shortwave_flux_in_air")) |>
+  dplyr::collect()
+dr = dr |> 
+  dplyr::filter(between(datetime,lubridate::as_datetime(as.Date(today)-jumpBack),lubridate::as_datetime(today)))  ## couldn't get between to work in initial query
+PAR = dr |>
+  filter(variable == "surface_downwelling_shortwave_flux_in_air") |>
+  na.omit() |>
+  pivot_wider(names_from = parameter,values_from = prediction) 
+inputs.reforecast <- list(
+  date = PAR$datetime,
+  temp = dr |> filter(variable == "air_temperature") |>
+    na.omit() |>
+    pivot_wider(names_from = parameter,values_from = prediction) |>
+    select(-(1:11)) %>% t()-273.15, ## air temperature (Celsius) ## prev included %>% head(-1)
+  PAR  = as.matrix(t(PAR[,-(1:11)])) / 0.486 ## gap filled PAR, conversion From Campbell and Norman p151
+)
 
 ## Reforecast with updated drivers
-out = ensemble_forecast(X = Analysis[[last]]$X,           ## initial conditions = yesterday's Analysis
-                        params = Analysis[[last]]$params, ## today's parameters = yesterday's Analysis
+out = ensemble_forecast(X = Analysis$X,           ## initial conditions = yesterday's Analysis
+                        params = Analysis$params, ## today's parameters = yesterday's Analysis
                         inputs = inputs.reforecast)       ## today's subset of meteorology 
 
 ## Assimilate most recent observations
@@ -74,7 +86,7 @@ out.f = ensemble_forecast(X = Analysis[[last+1]]$X,           ## initial conditi
 
 ## Convert Forecast to standard & save
 dimnames(out.f) <- list(as.character(date[today]),as.character(1:ne),varnames) ## label array dimensions
-fx = as.data.frame.table(out.f)                                                ## reorganize into long foremat
+fx = as.data.frame.table(out.f)                                                ## reorganize into long format
 colnames(fx) = c("time","ensemble","variable","prediction")                    ## label columns
 fx_file = file.path(outdir,
                     paste0("terrestrial_30min_",as.character(as.Date(today)),"_SSEM.csv")) ## output filename
@@ -84,8 +96,8 @@ write_csv(fx,fx_file)
 Sys.setenv("AWS_DEFAULT_REGION" = "data",
            "AWS_S3_ENDPOINT" = "ecoforecast.org")
 
-#aws.s3::put_object(object = fx_file, bucket = "submissions")
-#aws.s3::put_object(object = meta_file, bucket = "submissions")
+aws.s3::put_object(object = fx_file, bucket = "submissions")
+aws.s3::put_object(object = meta_file, bucket = "submissions")
 
 
 
