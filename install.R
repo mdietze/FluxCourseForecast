48a61520d410c6c4f97a160c3b234184a88abac7
Michael Dietze <dietze@bu.edu>
2023-06-14 16:11:55 -0400

    Tweaks to Rmd to save Analysis files individually to be able to better support forecast

--- install.R
+++ install.R
@@ -1,7 +1,13 @@
 list.of.packages <- c("arrow",
+                      "aws.s3",
                       "compiler", 
                       "EML",
                       "mvtnorm",
+                      "remotes",
                       "tidyverse")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
+
+if(!("neon4cast" %in% installed.packages()[,"Package"])){
+  remotes::install_github("eco4cast/neon4cast",upgrade="always")
+}
